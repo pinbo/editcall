d7e15ec0b84c0b1f06959ccf3fe06352423a31c0
Junli Zhang <zhjl86@gmail.com>
2022-05-08 22:06:36 -0700

    add all files

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # editcall
 Call indels and inversions for CRISPR NGS data processed with bwa-mem
+
+This tool can call big deletions and inversions from the split mapping (when there is `SA:Z:`tag) in the sam files from `bwa mem`.
+
+Some notes about `bwa mem` sam file:
+
+1. The primary alignment has soft clipping (S in the CIGAR), and the Supplementary alignment has hard clipping (H in the CIGAR).
+2. Unmapped reads have CIGAR "*"
+
+## Compile for local use
+You need to install [Nim](https://nim-lang.org/) compiler first. Then compile it with command:  
+`nim c -d:release nimBoxshadeMini.nim`
+
+Then check the usage by typing:  
+`./editcall`
+
+## Compile for webassembly
+I get the instruction on how to compile a Nim program to Webassembly [here](https://github.com/treeform/nim_emscripten_tutorial). You need to install [Emscripten](https://emscripten.org/docs/getting_started/downloads.html) first. Then use the command below to compile it:  
+`nim c -d:emscripten -o:editcall.js editcall.nim`
+
+Then you can use them as the way in [biowasm](https://github.com/biowasm/biowasm). You can pass more parameters to `emcc` in the last line of file `config.nims`.
+
+For some reason, this time "std/parseopt" did not work in the webassembly, so I have to use positioned based command line arguments. I also found it was not working to run the program multiple times in the browser. So I made it to run all the sam files in the same time (give all the sam file names to the command line).
+
+You can comment out the line `--define:release` in the file `config.nims` to debug it with `node`:
+
+```
+node editcall.js
+```
+
+## Usage
+
+```
+Usage: ./editcall reference.fa output.txt samfile1.sam samfile2.sam [...more samfiles]
+```
+You need to provide the reference fasta file that is used for `bwa mem`, the output file name, and all the sam files you want to check.
+
+Right now it only support sam files. If you have bam files, please run  
+`samtools view your.bam > your.sam`  
+to convert the bam to sam file.
---
+++ archived/call_indels3.nim
@@ -0,0 +1,319 @@
+## modules
+import std/tables
+import strutils
+# import std/parseopt
+import os
+# import std/re # regex, has split
+
+proc writeHelp() =
+  # quit("synopsis: " & getAppFilename() & " input.sam -g=reference.fa -o=output.txt")
+  quit("synopsis: ./editcall input.sam reference.fa output.txt")
+# proc writeVersion() =
+#   quit(getAppFilename() & " version 0.1.0")
+
+echo commandLineParams()
+echo paramCount(), " parameters" #, " ", paramStr(1)
+if paramCount() != 3:
+  writeHelp()
+
+## use regular positioned based arguments
+let
+  samFile = commandLineParams()[0]
+  refFile = commandLineParams()[1]
+  outfile = commandLineParams()[2]
+
+echo "outfile is ", outfile
+
+# var 
+#   samFile: string
+#   refFile: string
+#   outfile = "out.txt"
+# var p = initOptParser( commandLineParams() )
+# for kind, key, val in p.getopt():
+#   case kind
+#   of cmdArgument:
+#     samFile = key
+#   of cmdLongOption, cmdShortOption:
+#     case key
+#     of "help", "h": writeHelp()
+#     of "version", "v": writeVersion()
+#     of "g": refFile = val
+#     of "o": outfile = val
+#   of cmdEnd: assert(false) # cannot happen
+# if samFile == "":
+#   writeHelp()
+
+## use regular positioned based arguments
+
+
+# read a fasta file
+proc readFasta(infile: string): Table[string, string] =
+  # let contents = readFile(infile).strip() 
+  # echo contents
+  # let lines = contents.splitLines()
+  var seqDict = initTable[string, string]()
+  var seqName = ""
+  for ll in lines infile:
+    if contains(ll, ">"):
+      seqName = ll.replace(">", "")
+      seqDict[seqName] = ""
+    else:
+      seqDict[seqName].add(ll)
+  return seqDict
+
+## split cigar
+proc mysplit (cigar: string): (seq[char], seq[int]) =
+  # echo cigar # 8M1D108M
+  let num = "0123456789"
+  var cc: seq[char]
+  for i in cigar:
+    if i notin num:
+      cc.add(i)
+  # echo cc
+  var nn: seq[int]
+  var tmp = ""
+  for i in cigar:
+    if i in num:
+      tmp = tmp & i
+    else:
+      nn.add(tmp.parseInt)
+      tmp = ""
+  # echo nn
+  return (cc, nn)
+
+
+# process cigar and return
+# all positions are 0-based
+proc parseCigar (cigar: string, refPos: int, sameStrand: bool, readLen: int): seq[int] =
+  let (ss1, ss2) = cigar.mysplit #@["M", "D", "M", "S"], @["60", "5", "56", "26"]
+  # all positions are 1-based
+  var 
+    readPos1 = 0 # left start
+    readPos2 = -1 # right end
+    refPos1 = refPos # left start
+    refPos2 = refPos - 1 # right end
+    nM = 0 # number of M, if match showed up, then no more S or H
+  for i in 0 .. ss1.len - 1:
+    let num = ss2[i]
+    if ss1[i] == 'M' or ss1[i] == '=' or ss1[i] == 'X':
+      readPos2 += num
+      refPos2 += num
+      nM += 1
+    elif ss1[i] == 'S' or ss1[i] == 'H':
+      if nM == 0:
+        readPos1 += num
+        readPos2 += num
+    elif ss1[i] == 'I':
+      readPos2 += num
+    elif ss1[i] == 'D' or ss1[i] == 'N':
+      refPos2 += num - 1
+  if sameStrand:
+    return @[readPos1, readPos2, refPos1, refPos2]
+  else:
+    return @[readLen - readPos2 - 1, readLen - readPos1 - 1, refPos1, refPos2]
+
+## read fasta
+echo "start reading fasta"
+let seqDict = readFasta(refFile)
+echo "fasta file has been read!"
+# init dict for variations
+var indelDict = initTable[string, int]()
+
+## read files line by line
+for line in lines samFile:
+  if line[0] == '@':
+    continue
+  else:
+    let ff = line.split("\t")
+    let
+      flag   = ff[1].parseInt
+      chrom  = ff[2]
+      pos  = ff[3].parseInt - 1 # 0 based
+      cigar  = ff[5] 
+      readSeq= ff[9]
+    let strand = if (flag and 0x10) != 0: "-" else: "+"
+    # readID = ff[0]
+    # echo readID
+    var refSeq = ""
+    var altSeq = ""
+    var mutSize = 0
+    var kk = "" # key of indel dict
+    var readPos = 0 # relative positions for clipped reads
+    var rawReadPos = 0 # positions of the raw read before clipping
+    var refPos = pos # 0-based
+    # check if there is insertion or deletion
+    if ('I' in cigar or 'D' in cigar or 'N' in cigar) or ("SA:Z" in line and 'H' notin cigar):
+      let (ss1, ss2) = cigar.mysplit #@["M", "D", "M", "S"], @[60, 5, 56, 26]
+      for i in 0 .. ss1.len-1:
+        let num = ss2[i]
+        if ss1[i] == 'M' or ss1[i] == '=' or ss1[i] == 'X':
+          readPos += num
+          refPos += num
+          rawReadPos += num
+        elif ss1[i] == 'S':
+          readPos += num
+          rawReadPos += num
+        elif ss1[i] == 'H':
+          rawReadPos += num
+        elif ss1[i] == 'I':
+          refSeq = seqDict[chrom][refPos-1 .. refPos]
+          altSeq = readSeq[readPos-1 .. readPos+num]
+          mutSize = altSeq.len - refSeq.len
+          kk = @[chrom, $refPos, $(refPos+1), refSeq, altSeq, $mutSize].join("\t")
+          if kk in indelDict:
+            indelDict[kk] += 1
+          else:
+            indelDict[kk] = 1
+          readPos += num
+          rawReadPos += num
+        elif ss1[i] == 'D' or ss1[i] == 'N':
+          refSeq = seqDict[chrom][refPos-1 .. refPos+num]
+          altSeq = readSeq[readPos-1 .. readPos]
+          mutSize = altSeq.len - refSeq.len
+          kk = @[chrom, $refPos, $(refPos+num+1), refSeq, altSeq, $mutSize].join("\t")
+          if kk in indelDict:
+            indelDict[kk] += 1
+          else:
+            indelDict[kk] = 1
+          refPos += num
+    # check if having supplementary read
+    # SA:Z:6B,185,-,54S62M26S,10,0;
+    if "SA:Z" in line and 'H' notin cigar: # only when the read has no hard clip, eg. read is raw
+      let ff2 = line.split("SA:Z:")[1].split(",")
+      let 
+        saChrom  = ff2[0]
+        saPos  = ff2[1].parseInt - 1 # 0-based this is close to the border on the left, may need to adjust
+        saStrand = ff2[2]
+        saCigar  = ff2[3]
+      if chrom == saChrom and strand == saStrand:# and (flag and 0x20) == 0: # big deletion, could be insertion too, but update later
+        # echo "potential big deletion"
+        var allPos1: seq[int]
+        var allPos2: seq[int]
+        if (saPos > pos): # SA is on the right
+          allPos2 = parseCigar(saCigar, saPos, true, readSeq.len) # return readPos1, readPos2, refPos1, refPos2
+          allPos1 = parseCigar(cigar, pos, true, readSeq.len)
+        else:
+          allPos1 = parseCigar(saCigar, saPos, true, readSeq.len) # return readPos1, readPos2, refPos1, refPos2
+          allPos2 = parseCigar(cigar, pos, true, readSeq.len)
+        # echo allPos1
+        # echo allPos2
+        let
+          readPos1 = allPos1[1]
+          refPos1  = allPos1[3]
+          readPos2 = allPos2[0]
+          refPos2  = allPos2[2]
+          shift = if readPos1 >= readPos2: readPos1 - readPos2 + 1 else: 0
+          delEndPos = refPos2 + shift
+        # echo refPos1
+        # echo shift
+        # echo refPos2
+        # echo delEndPos
+        refSeq = seqDict[chrom][refPos1 .. delEndPos-1]
+        altSeq = readSeq[readPos1 .. readPos2+shift]
+        mutSize = altSeq.len - refSeq.len
+        kk = @[chrom, $(refPos1+1), $delEndPos, refSeq, altSeq, $mutSize].join("\t")
+        if kk in indelDict:
+          indelDict[kk] += 1
+        else:
+          indelDict[kk] = 1
+      elif chrom == saChrom and strand != saStrand: # inversions
+        # echo "Potential inversion"
+        var allPos1: seq[int]
+        var allPos2: seq[int]
+        if (saPos > pos): # SA is on the right
+          allPos1 = parseCigar(cigar, pos, true, readSeq.len) # return readPos1, readPos2, refPos1, refPos2
+          allPos2 = parseCigar(saCigar, saPos, false, readSeq.len)
+        else:
+          allPos1 = parseCigar(saCigar, saPos, false, readSeq.len)
+          allPos2 = parseCigar(cigar, pos, true, readSeq.len)
+        # echo allPos1
+        # echo allPos2
+        var
+          readPos1 = allPos1[1]
+          refPos1  = allPos1[3]
+          readPos2 = allPos2[1]
+          refPos2  = allPos2[3]
+          # shift = if readPos1 >= readPos2: readPos1 - readPos2 + 1 else: 0
+          # delEndPos = refPos2 - shift
+        if (flag and 0x80) != 0: # 2nd in pair, need to count from right
+          readPos1 = allPos2[0]
+          refPos1  = allPos1[2]
+          readPos2 = allPos1[0]
+          refPos2  = allPos2[2]
+        let shift = if readPos1 >= readPos2: readPos1 - readPos2 + 1 else: 0
+        let delEndPos = refPos2 - shift
+        # echo refPos1
+        # echo refPos2
+        # echo readPos1
+        # echo readPos2
+        # echo delEndPos
+        refSeq = seqDict[chrom][refPos1 .. delEndPos-1]
+        altSeq = "inversion"
+        mutSize = refSeq.len
+        kk = @[chrom, $(refPos1+1), $delEndPos, refSeq, altSeq, $mutSize].join("\t")
+        if kk in indelDict:
+          indelDict[kk] += 1
+        else:
+          indelDict[kk] = 1
+
+## print at the end
+let f = open(outfile, fmWrite)
+echo "outfile is opened for writing!"
+f.writeLine("Chrom\trefStartPos\trefEndPos\tRef\tAlt\tmutSize\tnMutRead")
+for k, v in indelDict:
+  f.writeLine(k & "\t" & $v)
+
+f.close()
+echo "Everything is done!"
+# use a variable
+# var fileContent = "Chrom\trefStartPos\trefEndPos\tRef\tAlt\tmutSize\tnMutRead\n"
+# for k, v in indelDict:
+#   fileContent.add(k & "\t" & $v & "\n")
+# writeFile(outfile, fileContent)
+# bitwise check example
+# proc bitwise(a: int, b: int) =
+#   echo "a and b: " , a and b
+#   echo "a or b: ", a or b
+#   echo "a xor b: ", a xor b
+#   echo "not a: ", not a
+#   echo "a << b: ", a shl b
+#   echo "a >> b: ", a shr b
+
+# bitwise(145, 0x40)
+
+# split cigar
+# import std/re # regex, has split
+# let cigar = "60M5D56M26S"
+# let ss1 = cigar.split(re"\d+")
+# echo ss1
+# let ss2 = cigar.split(re"[MIDNSHP=X]")
+# echo ss2
+
+# bitwise check example
+# proc flagcheck(a: int) =
+#   echo "   0x1     1  PAIRED         " , a and 0x1
+#   echo "   0x2     2  PROPER_PAIR    " , a and 0x2
+#   echo "   0x4     4  UNMAP          " , a and 0x4
+#   echo "   0x8     8  MUNMAP         " , a and 0x8
+#   echo "  0x10    16  REVERSE        " , a and 0x10
+#   echo "  0x20    32  MREVERSE       " , a and 0x20
+#   echo "  0x40    64  READ1          " , a and 0x40
+#   echo "  0x80   128  READ2          " , a and 0x80
+#   echo " 0x100   256  SECONDARY      " , a and 0x100
+#   echo " 0x200   512  QCFAIL         " , a and 0x200
+#   echo " 0x400  1024  DUP            " , a and 0x400
+#   echo " 0x800  2048  SUPPLEMENTARY  " , a and 0x800
+
+
+#    0x1     1  PAIRED         paired-end / multiple-segment sequencing technology
+#    0x2     2  PROPER_PAIR    each segment properly aligned according to aligner
+#    0x4     4  UNMAP          segment unmapped
+#    0x8     8  MUNMAP         next segment in the template unmapped
+#   0x10    16  REVERSE        SEQ is reverse complemented
+#   0x20    32  MREVERSE       SEQ of next segment in template is rev.complemented
+#   0x40    64  READ1          the first segment in the template
+#   0x80   128  READ2          the last segment in the template
+#  0x100   256  SECONDARY      secondary alignment
+#  0x200   512  QCFAIL         not passing quality controls or other filters
+#  0x400  1024  DUP            PCR or optical duplicate
+#  0x800  2048  SUPPLEMENTARY  supplementary alignment
